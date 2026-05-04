67ecee4e22f599b73399156f643d5cbdfe6f4f54
norurazak <noru.razak@gmail.com>
2026-05-04 22:39:10 +0800

    seo: add robots.ts file for search engine crawlers

---
+++ app/robot.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      // This tells all search engine bots (*) that they are allowed to scan your site
+      userAgent: '*',
+      allow: '/',
+    },
+    // This points the bots directly to the sitemap we updated earlier!
+    sitemap: 'https://fishyology.org/sitemap.xml',
+  };
+}
